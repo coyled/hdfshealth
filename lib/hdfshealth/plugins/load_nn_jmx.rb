d2a6600a2e7a7e7c598ee3c3b625c5b6bfd42fe1
Dave Coyle <hello@coyled.com>
2016-02-05 20:42:23 -0800

    flesh out LoadNNJMX

--- lib/hdfshealth/plugins/load_nn_jmx.rb
+++ lib/hdfshealth/plugins/load_nn_jmx.rb
@@ -1,15 +1,32 @@
 class LoadNNJMX < HDFSHealth::Plugin
 
+    require 'net/http'
+    require 'json'
+
     @jmx = {}
 
     def self.jmx(namenode)
-
         unless @jmx[namenode]
-            # fetch nn jmx here
-            #@jmx[namenode] = foo
+            self.fetch_jmx(namenode)
         end
 
         @jmx[namenode]
+    end
+
+    def self.fetch_jmx(namenode)
+        jmx =  Net::HTTP.get(URI("http://#{namenode}/jmx"))
+        stats = JSON.parse(jmx)
+
+        stats['beans'].each do |data|
+            self.parse_jmx(namenode, data)
+        end
+    end
+
+    def self.parse_jmx(namenode, data)
+
+        if data['name'] == 'Hadoop:service=NameNode,name=FSNamesystem'
+            @jmx[namenode] = { 'FSNamesystem' => data }
+        end
 
     end
 
