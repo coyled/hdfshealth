7127e21b1eb38ec2d0ca029e170cf75abb610381
Dave Coyle <hello@coyled.com>
2016-02-06 09:12:44 -0800

    readd spec/jmx/*.jmx files

--- spec/check_safe_mode_spec.rb
+++ spec/check_safe_mode_spec.rb
@@ -4,7 +4,16 @@ require 'hdfshealth'
 
 RSpec.describe CheckSafeMode do
     it "Verifies HDFS safe mode is off" do
-        test_jmx_file = 'file://' + File.dirname(__FILE__) + '/test_nn.jmx'
+        test_jmx_file = 'file://' + File.dirname(__FILE__) + '/jmx/nn_safe_mode_off.jmx'
+
+        check = CheckSafeMode.new
+        check.run(test_jmx_file)
+
+        expect(check.status).to eq('OK')
+    end
+
+    it "Verifies HDFS safe mode is on" do
+        test_jmx_file = 'file://' + File.dirname(__FILE__) + '/jmx/nn_safe_mode_on.jmx'
 
         check = CheckSafeMode.new
         check.run(test_jmx_file)
