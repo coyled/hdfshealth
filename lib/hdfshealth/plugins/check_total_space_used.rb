493bafe166f4293efec96772b0318dc43b594a43
Dave Coyle <hello@coyled.com>
2016-02-05 20:40:01 -0800

    plugins: add CheckTotalSpaceUsed

---
+++ lib/hdfshealth/plugins/check_total_space_used.rb
@@ -0,0 +1,23 @@
+class CheckTotalSpaceUsed < HDFSHealth::Plugin
+
+    require_relative 'load_nn_jmx'
+
+    def run(namenode)
+        jmx = LoadNNJMX.jmx(namenode)
+
+        total_space_used = jmx['FSNamesystem']['CapacityUsed'].to_i
+        total_space = jmx['FSNamesystem']['CapacityTotal'].to_i
+        pct_space_used = total_space_used.to_f / total_space.to_f
+
+        if pct_space_used < 0.5
+            @status = 'OK'
+        elsif pct_space_used < 0.7
+            @status = 'WARNING'
+        else
+            @status = 'CRITICAL'
+        end
+
+        @message = "#{'%.2f' % (pct_space_used * 100)}% of total space used (#{total_space_used} of #{total_space})"
+    end
+
+end
