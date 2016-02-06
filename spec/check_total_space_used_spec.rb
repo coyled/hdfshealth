bd10b5f26c5884d31c5f2e55b677a377c1209cd8
Dave Coyle <hello@coyled.com>
2016-02-06 08:20:58 -0800

    add tests for CheckTotalSpaceUsed

---
+++ spec/check_total_space_used_spec.rb
@@ -0,0 +1,23 @@
+$:.unshift File.join(File.dirname(__FILE__), '../lib')
+
+require 'hdfshealth'
+
+RSpec.describe CheckTotalSpaceUsed do
+    it "Verifies HDFS is using less than 60% of total space" do
+        test_jmx_file = 'file://' + File.dirname(__FILE__) + '/jmx/nn_ok_space_usage.jmx'
+
+        check = CheckTotalSpaceUsed.new
+        check.run(test_jmx_file)
+
+        expect(check.status).to eq('OK')
+    end
+
+    it "Verifies HDFS is using more than 60% of total space" do
+        test_jmx_file = 'file://' + File.dirname(__FILE__) + '/jmx/nn_high_space_usage.jmx'
+
+        check = CheckTotalSpaceUsed.new
+        check.run(test_jmx_file)
+
+        expect(check.status).to eq('WARNING')
+    end
+end
